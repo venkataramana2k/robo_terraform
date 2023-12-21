769d8e05ce7a837270400a27f934c991975b0111
sathya bhimuni <sathya.veer143@gmail.com>
2023-12-21 12:51:38 +0530

    project

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "instance" {
   for_each = var.component
-  source = "git::"
+  source = "git::https://github.com/venkataramana2k/robo_terraform_module.git"
   env = var.env
   component = each.key
 }
